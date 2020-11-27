cf241dc1f29cde676576cf5788576cd67864edf4
jhwangbo <jhwangbo@kaist.ac.kr>
2020-11-27 09:43:22 +0900

    updating python files

--- scripts/linux_build.sh
+++ scripts/linux_build.sh
@@ -1,7 +1,7 @@
 cd build
 
-for PY_VERSION in 3.5 3.6 3.7 3.8
+for PY_VERSION in 3.5 3.6 3.7 3.8 3.9
 do
 	cmake .. -DCMAKE_BUILD_TYPE=RELEASE -DRAISIM_MATLAB=ON -DRAISIM_PY=ON -DRAISIM_EXAMPLE=ON -DPYTHON_EXECUTABLE:FILEPATH=/usr/bin/python${PY_VERSION}
-  make -j
+    make -j
 done
