d54b2dfdb0cadfeb1882970d912f666bbefbcf5a
jhwangbo <jhwangbo@kaist.ac.kr>
2020-11-29 16:59:05 +0900

    constraint force stability+visualization improved

--- examples/src/server/newtonsCradle.cpp
+++ examples/src/server/newtonsCradle.cpp
@@ -38,6 +38,25 @@ int main(int argc, char **argv) {
   pin5->setPosition(0.9, 0.0, 6.0);
   pin5->setBodyType(raisim::BodyType::STATIC);
 
+  auto pin6 = world.addSphere(0.1, 0.8);
+  pin6->setPosition(-3., 0.0, 7.0);
+  pin6->setBodyType(raisim::BodyType::STATIC);
+  auto anymalC = world.addArticulatedSystem(binaryPath.getDirectory() + "\\rsc\\anymal_c\\urdf\\anymal.urdf");
+  /// anymalC joint PD controller
+  Eigen::VectorXd jointNominalConfig(anymalC->getGeneralizedCoordinateDim()), jointVelocityTarget(anymalC->getDOF());
+  jointNominalConfig << -3, 0, 4.54, 1.0, 0.0, 0.0, 0.0, 0.03, 0.4, -0.8, -0.03, 0.4, -0.8, 0.03, -0.4, 0.8, -0.03, -0.4, 0.8;
+  jointVelocityTarget.setZero();
+
+  Eigen::VectorXd jointPgain(anymalC->getDOF()), jointDgain(anymalC->getDOF());
+  jointPgain.tail(12).setConstant(100.0);
+  jointDgain.tail(12).setConstant(1.0);
+
+  anymalC->setGeneralizedCoordinate(jointNominalConfig);
+  anymalC->setGeneralizedForce(Eigen::VectorXd::Zero(anymalC->getDOF()));
+  anymalC->setPdGains(jointPgain, jointDgain);
+  anymalC->setPdTarget(jointNominalConfig, jointVelocityTarget);
+  anymalC->setName("anymalC");
+
   auto ball1 = world.addSphere(0.1498, 0.8, "steel");
   ball1->setPosition(0, 0.0, 1.0);
 
@@ -51,7 +70,7 @@ int main(int argc, char **argv) {
   ball4->setPosition(2.9, 0.0, 3.0);
 
   auto box = world.addBox(.1, .1, .1, 1);
-  box->setPosition(0.9, 0.0, 4.0);
+  box->setPosition(0.9, 0.0, 4.2);
 
   world.addStiffWire(pin1, 0, {0,0,0}, ball1, 0, {0,0,0}, 2.0);
   world.addStiffWire(pin2, 0, {0,0,0}, ball2, 0, {0,0,0}, 2.0);
@@ -59,13 +78,19 @@ int main(int argc, char **argv) {
   world.addStiffWire(pin4, 0, {0,0,0}, ball4, 0, {0,0,0}, 2.0);
 
   auto wire5 = world.addCompliantWire(pin5, 0, {0,0,0}, box, 0, {0., 0, 0}, 2.0, 200);
+  wire5->setStretchType(raisim::LengthConstraint::StretchType::BOTH);
+
+  auto wire6 = world.addCompliantWire(pin6, 0, {0,0,0}, anymalC, 0, {0., 0, 0}, 2.0, 1000);
+  wire6->setStretchType(raisim::LengthConstraint::StretchType::BOTH);
 
   /// launch raisim servear
   raisim::RaisimServer server(&world);
   server.launchServer();
 
-  while (1) {
-    raisim::MSLEEP(1);
+  for (int i=0; i< 50000; i++) {
+    std::this_thread::sleep_for(std::chrono::microseconds(1000));
     server.integrateWorldThreadSafe();
   }
+
+  server.killServer();
 }
