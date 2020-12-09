0aec364da5d5502a952c18d3e6e4cbfed841804b
Fan Shi <shifan@jsk.imi.i.u-tokyo.ac.jp>
2020-12-09 15:21:56 +0900

    Add tester to load and visualize trained policy.

---
+++ raisimGymTorch/raisimGymTorch/env/envs/rsg_anymal/tester.py
@@ -0,0 +1,77 @@
+from ruamel.yaml import YAML, dump, RoundTripDumper
+from raisimGymTorch.env.bin import rsg_anymal
+from raisimGymTorch.env.RaisimGymVecEnv import RaisimGymVecEnv as VecEnv
+from raisimGymTorch.helper.raisim_gym_helper import ConfigurationSaver
+import os
+import math
+import time
+import raisimGymTorch.algo.ppo.module as ppo_module
+import raisimGymTorch.algo.ppo.ppo as PPO
+import torch.nn as nn
+import numpy as np
+import torch
+import datetime
+import argparse
+
+# configuration
+parser = argparse.ArgumentParser()
+parser.add_argument('-w', '--weight', help='trained weight path', type=str, default='')
+args = parser.parse_args()
+
+# directories
+task_path = os.path.dirname(os.path.realpath(__file__))
+home_path = task_path + "/../../../../.."
+
+# config
+cfg = YAML().load(open(task_path + "/cfg.yaml", 'r'))
+
+# create environment from the configuration file
+cfg['environment']['num_envs'] = 1
+
+env = VecEnv(rsg_anymal.RaisimGymEnv(home_path + "/rsc", dump(cfg['environment'], Dumper=RoundTripDumper)), cfg['environment'])
+
+# shortcuts
+ob_dim = env.num_obs
+act_dim = env.num_acts
+
+weight_path = args.weight
+if weight_path == "":
+    print("Can't find trained weight, please provide a trained weight with --weight switch\n")
+else:
+    print("Loaded weight from {}\n".format(weight_path))
+    start = time.time()
+    env.reset()
+    reward_ll_sum = 0
+    done_sum = 0
+    average_dones = 0.
+    n_steps = math.floor(cfg['environment']['max_time'] / cfg['environment']['control_dt'])
+    total_steps = n_steps * 1
+    start_step_id = 0
+
+    print("Visualizing and evaluating the policy", weight_path+".pt")
+    loaded_graph = torch.jit.load(weight_path+'.pt')
+
+    env.load_scaling(weight_path.rsplit('/', 1)[0], int(weight_path.rsplit('/', 1)[1].split('_', 1)[1]))
+    print("Load observation scaling in", weight_path.rsplit('/', 1)[0]+":", "mean"+str(int(weight_path.rsplit('/', 1)[1].split('_', 1)[1])) + ".csv", "and", "var"+str(int(weight_path.rsplit('/', 1)[1].split('_', 1)[1])) + ".csv")
+    env.turn_on_visualization()
+
+    # max_steps = 1000000
+    max_steps = 1000 ## 10 secs
+    for step in range(max_steps):
+        time.sleep(0.01)
+        obs = env.observe(False)
+        action_ll = loaded_graph(torch.from_numpy(obs).cpu())
+        reward_ll, dones = env.step(action_ll.cpu().detach().numpy())
+        reward_ll_sum = reward_ll_sum + reward_ll[0]
+        if dones or step == max_steps - 1:
+            print('----------------------------------------------------')
+            print('{:<40} {:>6}'.format("sum reward: ", '{:0.10f}'.format(reward_ll_sum)))
+            print('{:<40} {:>6}'.format("average ll reward: ", '{:0.10f}'.format(reward_ll_sum / (step + 1 - start_step_id))))
+            print('{:<40} {:>6}'.format("time elapsed [sec]: ", '{:6.4f}'.format((step + 1 - start_step_id) * 0.01)))
+            print('----------------------------------------------------\n')
+            start_step_id = step + 1
+            reward_ll_sum = 0.0
+
+    env.turn_off_visualization()
+    env.reset()
+    print("Finished at the maximum visualization steps")
