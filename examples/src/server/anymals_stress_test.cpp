5818fe22793de2bfc528273857a7170a31467b45
Jemin <jemin.hwangbo@gmail.com>
2021-02-17 08:41:14 +0900

    adding anymal stress test

---
+++ examples/src/server/anymals_stress_test.cpp
@@ -0,0 +1,58 @@
+// This file is part of RaiSim. You must obtain a valid license from RaiSim Tech
+// Inc. prior to usage.
+
+#include "raisim/RaisimServer.hpp"
+#include "raisim/World.hpp"
+#include <random>
+
+int main(int argc, char* argv[]) {
+  auto binaryPath = raisim::Path::setFromArgv(argv[0]);
+  raisim::World::setActivationKey(binaryPath.getDirectory() + "\\rsc\\activation.raisim");
+
+  /// create raisim world
+  raisim::World world;
+  world.setTimeStep(0.001);
+
+  /// create objects
+  world.addGround();
+  auto anymalB = world.addArticulatedSystem(binaryPath.getDirectory() + "\\rsc\\anymal\\urdf\\anymal.urdf");
+  auto anymalC = world.addArticulatedSystem(binaryPath.getDirectory() + "\\rsc\\anymal_c\\urdf\\anymal.urdf");
+
+  /// anymalC joint PD controller
+  Eigen::VectorXd jointNominalConfig(anymalC->getGeneralizedCoordinateDim()), jointVelocityTarget(anymalC->getDOF());
+  jointNominalConfig << 0, 0, 0.54, 1.0, 0.0, 0.0, 0.0, 0.03, 0.4, -0.8, -0.03, 0.4, -0.8, 0.03, -0.4, 0.8, -0.03, -0.4, 0.8;
+  jointVelocityTarget.setZero();
+
+  anymalC->setGeneralizedCoordinate(jointNominalConfig);
+  anymalC->setGeneralizedForce(Eigen::VectorXd::Zero(anymalC->getDOF()));
+  anymalC->setName("anymalC");
+
+  jointNominalConfig[1] = 1.0;
+  anymalB->setGeneralizedCoordinate(jointNominalConfig);
+  anymalB->setGeneralizedForce(Eigen::VectorXd::Zero(anymalB->getDOF()));
+  anymalB->setName("anymalB");
+
+  /// launch raisim servear
+  raisim::RaisimServer server(&world);
+  server.launchServer();
+  server.focusOn(anymalC);
+
+  double lower_bound = -80;
+  double upper_bound = 80;
+  std::uniform_real_distribution<double> unif(lower_bound,upper_bound);
+  std::default_random_engine re;
+  Eigen::VectorXd genForce(18);
+
+  for (int i=0; i<200000000; i++) {
+    std::this_thread::sleep_for(std::chrono::microseconds(1000));
+    if(i%2000==0)
+      for(int j=0; j<18; j++)
+        genForce[j] = unif(re);
+
+    anymalB->setGeneralizedForce(genForce);
+    anymalC->setGeneralizedForce(genForce);
+    server.integrateWorldThreadSafe();
+  }
+
+  server.killServer();
+}
