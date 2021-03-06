e1fad42965261d556f9572e09c08fb78d0f108f6
Jemin Hwangbo <jhwangbo@ethz.ch>
2021-03-06 14:43:23 +0900

    examples now have a correct timer in windows

--- examples/src/server/anymals_stress_test.cpp
+++ examples/src/server/anymals_stress_test.cpp
@@ -4,10 +4,16 @@
 #include "raisim/RaisimServer.hpp"
 #include "raisim/World.hpp"
 #include <random>
+#if WIN32
+#include <timeapi.h>
+#endif
 
 int main(int argc, char* argv[]) {
   auto binaryPath = raisim::Path::setFromArgv(argv[0]);
   raisim::World::setActivationKey(binaryPath.getDirectory() + "\\rsc\\activation.raisim");
+#if WIN32
+    timeBeginPeriod(1); // for sleep_for function. windows default clock speed is 1/64 second. This sets it to 1ms.
+#endif
 
   /// create raisim world
   raisim::World world;
--- examples/src/server/balls.cpp
+++ examples/src/server/balls.cpp
@@ -3,10 +3,16 @@
 
 #include "raisim/RaisimServer.hpp"
 #include "raisim/World.hpp"
+#if WIN32
+#include <timeapi.h>
+#endif
 
 int main(int argc, char* argv[]) {
   auto binaryPath = raisim::Path::setFromArgv(argv[0]);
   raisim::World::setActivationKey(binaryPath.getDirectory() + "\\rsc\\activation.raisim");
+#if WIN32
+    timeBeginPeriod(1); // for sleep_for function. windows default clock speed is 1/64 second. This sets it to 1ms.
+#endif
 
   /// create raisim world
   double dt = 0.003;
