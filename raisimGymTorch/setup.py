1876ed8d98b435acac9fc57fceb7495bb6ad919d
Jemin Hwangbo <jhwangbo@ethz.ch>
2021-12-07 19:11:40 +0900

    fixed the build type in win

--- raisimGymTorch/setup.py
+++ raisimGymTorch/setup.py
@@ -45,7 +45,7 @@ class CMakeBuild(build_ext):
         if __CMAKE_PREFIX_PATH__ is not None:
             cmake_args.append('-DCMAKE_PREFIX_PATH=' + __CMAKE_PREFIX_PATH__)
 
-        cfg = 'Debug' if __DEBUG__ else 'RelWithDebInfo'
+        cfg = 'Debug' if __DEBUG__ else 'Release'
         build_args = ['--config', cfg]
 
         if platform.system() == "Windows":
