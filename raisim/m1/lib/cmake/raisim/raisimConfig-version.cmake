908c16796544ee0bc15a6585040e0d68449c22bc
Jemin Hwangbo <jhwangbo@kaist.ac.kr>
2023-03-31 10:58:32 +0900

    m1 update

--- raisim/m1/lib/cmake/raisim/raisimConfig-version.cmake
+++ raisim/m1/lib/cmake/raisim/raisimConfig-version.cmake
@@ -7,7 +7,7 @@
 # PACKAGE_VERSION_COMPATIBLE if the current version is >= requested version.
 # The variable CVF_VERSION must be set before calling configure_file().
 
-set(PACKAGE_VERSION "1.1.6")
+set(PACKAGE_VERSION "1.1.7")
 
 if (PACKAGE_FIND_VERSION_RANGE)
   # Package version must be in the requested version range
