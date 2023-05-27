14786bf7bf56b93ae95deacf4945531b6df63cc7
jemin <jhwangbo@kaist.ac.kr>
2023-05-27 10:39:23 +0900

    python updates

--- DEVELOPERS_ONLY/linux_build.sh
+++ DEVELOPERS_ONLY/linux_build.sh
@@ -1,9 +1,7 @@
-cd build
-
-for PY_VERSION in 3.8 3.9
+for PY_VERSION in 3.8 3.9 3.10
 do
-	CXX=/usr/bin/clang++-11 CC=/usr/bin/clang-11 cmake .. -DCMAKE_BUILD_TYPE=RELEASE -DRAISIM_MATLAB=OFF -DRAISIM_PY=ON -DRAISIM_EXAMPLE=OFF -DRAISIM_DOC=OFF -DPYTHON_EXECUTABLE:FILEPATH=/usr/bin/python${PY_VERSION}
-    make -j
+	CXX=/usr/bin/clang++-11 CC=/usr/bin/clang-11 cmake .. -DCMAKE_BUILD_TYPE=RELEASE -DRAISIM_MATLAB=OFF -DRAISIM_PY=ON -DRAISIM_EXAMPLE=OFF -DCMAKE_INSTALL_PREFIX=~/worksapce/raisim_install -DRAISIM_DOC=OFF -DPYTHON_EXECUTABLE:FILEPATH=/usr/bin/python${PY_VERSION}
+  make install -j
 done
 
 CXX=/usr/bin/clang++-11 CC=/usr/bin/clang-11 cmake .. -DCMAKE_BUILD_TYPE=RELEASE -DRAISIM_EXAMPLE=ON -DRAISIM_DOC=ON -DRAISIM_PY=OFF -DRAISIM_MATLAB=ON -DPYTHON_EXECUTABLE:FILEPATH=/home/jemin/anaconda3/envs/raisim/bin/python
--- DEVELOPERS_ONLY/linux_requirements.sh
+++ DEVELOPERS_ONLY/linux_requirements.sh
@@ -1,4 +1,4 @@
-for PY_VERSION in 3.8 3.9
+for PY_VERSION in 3.7 3.8 3.9 3.10
 do
   sudo apt-get -y install python$PY_VERSION libpython$PY_VERSION-dev python$PY_VERSION-distutils
 done
