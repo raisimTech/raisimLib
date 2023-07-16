0a63ab80c28a7533b26a7f0b43ff65be7cd1eeb5
jemin <jhwangbo@kaist.ac.kr>
2023-07-16 13:10:47 +0900

    linux update

--- DEVELOPERS_ONLY/linux_build.sh
+++ DEVELOPERS_ONLY/linux_build.sh
@@ -1,6 +1,6 @@
-for PY_VERSION in 3.8 3.9 3.10
+for PY_VERSION in 37 38 39 310 311
 do
-	CXX=/usr/bin/clang++-11 CC=/usr/bin/clang-11 cmake .. -DCMAKE_BUILD_TYPE=RELEASE -DRAISIM_MATLAB=OFF -DRAISIM_PY=ON -DRAISIM_EXAMPLE=OFF -DCMAKE_INSTALL_PREFIX=~/worksapce/raisim_install -DRAISIM_DOC=OFF -DPYTHON_EXECUTABLE:FILEPATH=/usr/bin/python${PY_VERSION}
+  CXX=/usr/bin/clang++-11 CC=/usr/bin/clang-11 cmake .. -DCMAKE_BUILD_TYPE=RELEASE -DRAISIM_MATLAB=OFF -DRAISIM_PY=ON -DRAISIM_EXAMPLE=OFF -DCMAKE_INSTALL_PREFIX=~/workspace/raisim_install -DRAISIM_DOC=OFF -DPYTHON_EXECUTABLE=/home/jemin/anaconda3/envs/py${PY_VERSION}/bin/python
   make install -j
 done
 
