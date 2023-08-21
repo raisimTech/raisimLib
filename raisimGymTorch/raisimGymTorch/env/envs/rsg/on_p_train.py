ef6931ad7e23c4b0369e72e224fb9d70e243c931
Lr-2002 <2629651228@qq.com>
2023-08-21 15:21:36 +0800

    this version is able to wheel with real time in raisim

--- raisimGymTorch/raisimGymTorch/env/envs/rsg/on_p_train.py
+++ raisimGymTorch/raisimGymTorch/env/envs/rsg/on_p_train.py
@@ -1,3 +1,6 @@
+import concurrent.futures
+import threading
+
 from ruamel.yaml import YAML, dump, RoundTripDumper
 from raisimGymTorch.env.RaisimGymVecEnv import RaisimGymVecEnv as VecEnv
 from raisimGymTorch.helper.raisim_gym_helper import ConfigurationSaver, load_param, tensorboard_launcher, RaisimLogger
@@ -79,6 +82,41 @@ saver = ConfigurationSaver(log_dir=home_path + "/raisimGymTorch/data/"+task_name
                            save_items=[task_path + "/cfg.yaml", task_path + "/Environment.hpp"])
 logger = RaisimLogger(saver.data_dir+"/train.log")
 
+
+
+def updating():
+    obs = env.observe(False)
+    ppo.update(actor_obs=obs, value_obs=obs, log_this_iteration=update % 2 == 0, update=update)
+    average_ll_performance = reward_sum / total_steps
+
+
+    if update % cfg['environment']['eval_every_n'] == 0:
+        biggest_iter = update
+        biggest_reward = average_ll_performance
+        torch.save({
+            'actor_architecture_state_dict': actor.architecture.state_dict(),
+            'actor_distribution_state_dict': actor.distribution.state_dict(),
+            'critic_architecture_state_dict': critic.architecture.state_dict(),
+            'optimizer_state_dict': ppo.optimizer.state_dict(),
+        }, saver.data_dir + "/full_" + str(update) + '.pt')
+        env.save_scaling(saver.data_dir, str(update))
+        save_act.save()
+        save_observe.save()
+
+
+    avg_rewards.append(average_ll_performance)
+
+    actor.update()
+    actor.distribution.enforce_minimum_std((torch.ones(12)).to(device))
+
+
+    print('----------------------------------------------------')
+    print('{:>6}th iteration'.format(update))
+    print('{:<40} {:>6}'.format("average ll reward: ", '{:0.10f}'.format(average_ll_performance)))
+    print('----------------------------------------------------\n')
+
+
+
 if mode =='train' or mode == 'retrain':
     tensorboard_launcher(saver.data_dir+"/..")  # press refresh (F5) after the first ppo update
 num_envs = cfg['environment']['num_envs']
@@ -90,11 +128,9 @@ ppo = PPO.PPO(actor=actor,
               gamma=0.996,
               lam=0.95,
               num_mini_batches=16,
-              # learning_rate_schedule='',
               device=device,
               log_dir=saver.data_dir,
               shuffle_batch=False,
-              # learning_rate=cfg['environment']['learnning_rate']
               )
 
 reward_analyzer = RewardAnalyzer(env, ppo.writer)
@@ -103,21 +139,16 @@ biggest_iter = 0
 if mode == 'retrain':
     load_param(weight_path, env, actor, critic, ppo.optimizer, saver.data_dir)
 
-# his_util=[0, 0.523, -1.046] * 4
-his_util=[0] * 12
-check_done = lambda a, b: a + 1 if not b else 0
-check_history = lambda a, b: a if not b else his_util
-
 save_observe = CSV_saver('./observation_saver')
 save_act = CSV_saver('./act_saver')
-
 print = logger.info
+
+
 on_p_rate =cfg['on_policy']['rate']
 on_p_kb = cfg['on_policy']['kb']
-env_idx = np.zeros(num_envs)
+
 mode == 'train'
-history_act = np.array([his_util] * num_envs)
-history_act_0 = np.array([his_util] * num_envs)
+
 total_update = args.update
 if mode =='train' or mode == 'retrain':
     env.turn_on_visualization()
@@ -126,7 +157,6 @@ if mode =='train' or mode == 'retrain':
     waiter = Waiter(0.01)
     waiter.update_start()
     for update in range(total_update):
-        # print('after update')
         reward_sum = 0
 
         for step in range(n_steps):
@@ -146,43 +176,13 @@ if mode =='train' or mode == 'retrain':
             reward_analyzer.add_reward_info(env.get_reward_info())
             reward_analyzer.analyze_and_plot(update)
         # take st step to get value obs
-        print('before update')
-        obs = env.observe(False)
-        ppo.update(actor_obs=obs, value_obs=obs, log_this_iteration=update % 2 == 0, update=update)
-        average_ll_performance = reward_sum / total_steps
-
-        print('before saving')
-
-        if update % cfg['environment']['eval_every_n'] == 0:
-            biggest_iter = update
-            biggest_reward = average_ll_performance
-            torch.save({
-                'actor_architecture_state_dict': actor.architecture.state_dict(),
-                'actor_distribution_state_dict': actor.distribution.state_dict(),
-                'critic_architecture_state_dict': critic.architecture.state_dict(),
-                'optimizer_state_dict': ppo.optimizer.state_dict(),
-            }, saver.data_dir+"/full_"+str(update)+'.pt')
-            env.save_scaling(saver.data_dir, str(update))
-            save_act.save()
-            save_observe.save()
-        print('after save')
-
-        avg_rewards.append(average_ll_performance)
-
-        actor.update()
-        actor.distribution.enforce_minimum_std((torch.ones(12)).to(device))
-        print('after update')
-
-        print('----------------------------------------------------')
-        print('{:>6}th iteration'.format(update))
-        print('{:<40} {:>6}'.format("average ll reward: ", '{:0.10f}'.format(average_ll_performance)))
-        # print('{:<40} {:>6}'.format("dones: ", '{:0.6f}'.format(average_dones)))
-        # print('{:<40} {:>6}'.format("time elapsed in this iteration: ", '{:6.4f}'.format(end - start)))
-        # print('{:<40} {:>6}'.format("fps: ", '{:6.0f}'.format(total_steps / (end - start))))
-        # print('{:<40} {:>6}'.format("real time factor: ", '{:6.0f}'.format(total_steps / (end - start)
-        #                                                                    * cfg['environment']['control_dt'])))
-        print('----------------------------------------------------\n')
-        print('after print')
+        update_thread = threading.Thread(target=updating)
+        update_thread.start()
+        while update_thread.is_alive():
+            waiter.wait()
+            # print('threading running')
+            env.step(action)
+        print('updating finished')
     env.turn_off_visualization()
 print(f'biggest:{biggest_reward},rate = {biggest_iter}')
 
