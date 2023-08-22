34e041a6cddc010676740a00e9a0737b3e52da06
Lr-2002 <2629651228@qq.com>
2023-08-22 18:53:02 +0800

    inverse turning data here

--- raisimGymTorch/raisimGymTorch/deploy_log/anay.py
+++ raisimGymTorch/raisimGymTorch/deploy_log/anay.py
@@ -1,14 +1,20 @@
-# import re
-# with open("./08-11-15-28", 'r') as f:
-#     lines = f.readlines()
-#     for line in lines:
-#         # x = re.search("work_action", line)
-#         # print(x)
-#         if "work_action" in line:
-#             print(line)
-
-
+import re
 import pandas as pd
+ans_list = []
 import numpy as np
-import matplotlib.pyplot as plt
+with open("./train.log", 'r') as f:
+    lines = f.readlines()
+    for line in lines:
+        # x = re.search("work_action", line)
+        # print(x)
+        if "average" in line:
+            line = line.strip()
+            # print(line[-20:])
+            x = float(line[-20:])
+            ans_list.append(x)
+
+
+df = np.array(ans_list)
+dd = pd.DataFrame(df).to_csv('./tss.csv')
+
 
