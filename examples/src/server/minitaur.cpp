3a171d9765146b9802a2e572404f2d1055eadb17
jemin <jhwangbo@kaist.ac.kr>
2024-02-21 06:55:57 +0900

    added more details to minitaur example

--- examples/src/server/minitaur.cpp
+++ examples/src/server/minitaur.cpp
@@ -28,8 +28,17 @@ int main(int argc, char* argv[]) {
   jointVelocityTarget.setZero();
 
   Eigen::VectorXd jointPgain(minitaur->getDOF()), jointDgain(minitaur->getDOF());
-  jointPgain.tail(16).setConstant(10.0); /// you must set the gains of the unactuated joints to zero. But here we make it simple
-  jointDgain.tail(16).setConstant(0.1);
+  /// we set the gains of the unactuated joints to zero
+  jointPgain << 0,0,0, 0,0,0,
+      20,0.,20,0.,
+      20,0.,20,0.,
+      20,0.,20,0.,
+      20,0.,20,0.;
+  jointDgain << 0,0,0, 0,0,0,
+      0.1,0.,0.1,0.,
+      0.1,0.,0.1,0.,
+      0.1,0.,0.1,0.,
+      0.1,0.,0.1,0.;
 
   minitaur->setGeneralizedCoordinate(jointNominalConfig);
   minitaur->setGeneralizedForce(Eigen::VectorXd::Zero(minitaur->getDOF()));
@@ -41,8 +50,6 @@ int main(int argc, char* argv[]) {
   raisim::RaisimServer server(&world);
   server.launchServer();
   server.focusOn(minitaur);
-//  world.setERP(0,0);
-//  world.setContactSolverParam(1.,1.,1.,2000,1e-8);
 
   for (int i=0; i<2000000; i++) {
     RS_TIMED_LOOP(1e6*world.getTimeStep())
